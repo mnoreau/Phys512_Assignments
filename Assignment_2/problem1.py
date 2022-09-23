0f37b81fba4271ceb9ef18319d18be0e9e835b58
mnoreau <73258318+mnoreau@users.noreply.github.com>
2022-09-23 00:56:21 -0400

    Problem 2

--- Assignment_2/problem1.py
+++ Assignment_2/problem1.py
@@ -1,3 +1,20 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy import integrate
+from scipy import integrate, interpolate
+
+def myintegration(x,y, step):
+
+
+
+
+def sciintegration(x,y,step):
+
+
+
+R=
+s=
+
+f = lambda :
+
+xdat = np.linspace(-1,1,101)
+ydat = f(xdat)z
